010a56d37cd40bff9702c4e0d5f310be5f1348e5
Suryanshtiwari2005 <148547496+Suryanshtiwari2005@users.noreply.github.com>
2024-06-09 11:15:44 +0530

    Variables in JAVA

---
+++ Variables.java
@@ -0,0 +1,22 @@
+
+public class Variables {
+    public static void main(String args[]) {
+        byte a = 127; // can only store one No.
+        int b = 26;
+        float c = 23.9f;
+        double d = 45.999;
+        char e = 'A';
+        long f = 2423424l;
+        short g = 45;
+        boolean h = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
